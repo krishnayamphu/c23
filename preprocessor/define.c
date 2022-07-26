fcfb422c4e9fe8d1e9f18225da638b203fd3584f
KrishnaYamphu <krishnayamphu@gmail.com>
2022-07-26 15:01:05 +0545

    define

---
+++ preprocessor/define.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#define PI 3.14
+#define NAME "Aptech"
+#define POW(n) n*n
+int main(){
+
+printf("PI: %.2f",PI);
+printf("\nName: %s",NAME);
+printf("\nResult: %d",POW(5));
+
+getch();
+return 0;
+}
